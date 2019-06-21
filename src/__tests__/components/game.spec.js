157be9f6ca39f8600434cdfb3f3403d12dbbc525
Lucas Assis Vieira <lucasav2005@hotmail.com>
2019-06-21 16:50:16 -0300

    Subindo algumas coisas finais

--- src/__tests__/components/game.spec.js
+++ src/__tests__/components/game.spec.js
@@ -2,9 +2,35 @@ import React from "react";
 import { mount } from "enzyme";
 
 import Game from "../../components/game";
+import MOCKED_DATA from "../../products.json";
+import { Provider } from "react-redux";
+import createStore from "redux-mock-store";
+
+import { actionCreators as CartCreators } from "../../store/ducks/cart";
+
+const mockStore = createStore();
+
+const INITIAL_STATE = {
+  games: MOCKED_DATA,
+  filter: null
+};
+
+const store = mockStore(INITIAL_STATE);
 
 it("sould render the game component", () => {
-  const wrapper = mount(<Game />);
+  const game = {
+    id: 312,
+    name: "Super Mario Odyssey",
+    price: 197.88,
+    score: 100,
+    image: "super-mario-odyssey.png"
+  };
 
-  expect(wrapper.find("img").exists()).toBe(true);
+  const wrapper = mount(
+    <Provider store={store}>
+      <Game game={game} />
+    </Provider>
+  );
+
+  expect(store.getActions()).toContainEqual(CartCreators.addItemOnCart(game));
 });
